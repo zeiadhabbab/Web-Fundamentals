91e3ad94fd1a8871a325177af90571f28f4c2820
Zeyad Habbab <zeiad.habbab@live.com>
2025-11-30 09:01:55 +0200

    First React App

---
+++ react/react-1/src/App.tsx
@@ -0,0 +1,19 @@
+import Description from "./components/Description";
+import Title from "./components/Title";
+import { GoodbyeWorld, HelloWorld } from "./components/Mutiple";
+
+
+function App() {
+
+  return (
+    <>
+      <Title />
+      <Description />
+      <HelloWorld />
+      <GoodbyeWorld />
+      <div></div>
+    </>
+  )
+}
+
+export default App
